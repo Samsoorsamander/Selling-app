14d16ed1bcfe9a74917d3b9cc33f696a8cbadecf
Samsoorsamander <samsoorsamander@gmail.com>
2024-12-04 20:09:35 +0430

    add ImageComponent Prop to itemList component

--- Components/ListItem.js
+++ Components/ListItem.js
@@ -3,7 +3,7 @@ import { View, StyleSheet, Image, TouchableHighlight } from "react-native";
 import AppText from "./AppText";
 import colors from "../config/colors";
 
-export default function ListItem({title, subTitle, image, onPress,renderRightAction}) {
+export default function ListItem({title, subTitle,ImageComponent, image, onPress,renderRightAction}) {
     return (
         <>
      <Swipeable
@@ -16,10 +16,11 @@ export default function ListItem({title, subTitle, image, onPress,renderRightAct
         >
             
         <View style= {styles.container}>
-            <Image style= {styles.image} source={image} />
-            <View>
+            {ImageComponent}
+            {image && <Image style= {styles.image} source={image} />}
+            <View style={styles.detailsContainer}>
             <AppText style={styles.title}>{title}</AppText>
-            <AppText style={styles.subTitle}>{subTitle}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
             </View>
         </View>
         </TouchableHighlight>
@@ -38,6 +39,11 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: "row",
         padding: 15
+    },
+    detailsContainer: {
+        marginLeft: 10,
+        justifyContent: "center",
+        alignItems: "center"
     },
     title: {
         fontWeight:"500",
--- app/index.js
+++ app/index.js
@@ -1,15 +1,15 @@
 import Screen from "./screens/Screen"
 import Icons from "../Components/Icons"
+import ListItem from "../Components/ListItem"
+import {GestureHandlerRootView} from "react-native-gesture-handler"
 
 export default function App() {
     return (
         <Screen>
-            <Icons
-            name="email"
-            size={50}
-            backgroundColor="red"
-            IconColor="white" 
-            />
+            <GestureHandlerRootView>
+            <ListItem title="MyTitle" ImageComponent={<Icons name="email"/>}/>
+                
+            </GestureHandlerRootView>            
         </Screen>
      
         )

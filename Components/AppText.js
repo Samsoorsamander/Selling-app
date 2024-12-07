447b44e881b4062e579f0dc6c3988862b95ae198
Samsoorsamander <samsoorsamander@gmail.com>
2024-12-07 08:22:52 +0430

    extract the default styles

--- Components/AppText.js
+++ Components/AppText.js
@@ -1,16 +1,9 @@
 import {  Text, StyleSheet, Platform } from "react-native";
 import colors from "../config/colors";
-
+import defaultStyles from "../config/styles";
 export default function AppText({children, style}) {
     return(
-        <Text style ={[styles.text, style]}>{children}</Text>
+        <Text style ={[defaultStyles.text, style]}>{children}</Text>
     )
 }
 
-const styles =StyleSheet.create({
-    text: {
-        color:colors.dark,
-        fontSize: 20,
-        fontFamily: Platform.OS ==="android" ? "Roboto" :"Avenir"
-    }
-})
--- Components/AppTextInput.js
+++ Components/AppTextInput.js
@@ -1,19 +1,18 @@
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { View, StyleSheet, TextInput, Platform } from "react-native";
-import colors from "../config/colors";
-
+import defaultStyles from"../config/styles"
 export default function AppTextInput({icon, ...otherProps}) {
     return (
         <View style={styles.container}>
-           { icon && <MaterialCommunityIcons name={icon} size={20} color={colors.medium} style={styles.icon}/> }
-            <TextInput style={styles.textInput} {...otherProps} />
+           { icon && <MaterialCommunityIcons name={icon} size={20} color={defaultStyles.colors.medium} style={styles.icon}/> }
+            <TextInput style={defaultStyles.text} {...otherProps} />
         </View>
     )
 }
 
 const styles =StyleSheet.create({
     container: {
-        backgroundColor:colors.light,
+        backgroundColor:defaultStyles.colors.light,
         borderRadius: 25,
         width:"100%",
         flexDirection: 'row',
@@ -22,11 +21,7 @@ const styles =StyleSheet.create({
         alignItems: 'center'
  
     },
-    textInput: {
-        fontSize: 18,
-        fontFamily:Platform.OS ==="android" ? "Roboto" :"Avenir"
-
-    },
+    
     icon: {
         marginRight: 10,
 

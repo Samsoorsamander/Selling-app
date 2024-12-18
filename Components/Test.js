4f0ba10a48de0898af283d7e21f75fd505c4dd5b
Samsoorsamander <samsoorsamander@gmail.com>
2024-12-18 09:42:19 +0430

    Custom Snippets

---
+++ Components/Test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+function Test(props) {
+return (
+<View style={Styles.container}></View>
+)
+}
+
+const Styles = StyleSheet.create({
+container: {
+
+}
+})
+export default Test;

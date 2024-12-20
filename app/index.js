8f6a50a6a273f27f712944921aa00e96ba4620ca
Samsoorsamander <samsoorsamander@gmail.com>
2024-12-20 19:54:35 +0430

    improve listEditScreen and form component

--- app/index.js
+++ app/index.js
@@ -1,25 +1,9 @@
-import {  useState } from "react";
-import Screen from "./screens/Screen";
-import ImageInputList from "../Components/ImageInputList";
+import ListEditScreen from "./screens/ListEditScreen";
 export default function App() {
-    const [imageUris, setImageUris] = useState([]);
-
-    const handleAdd = (uri) => { 
-        setImageUris([...imageUris, uri])
-    }
-    const handleRemove = (uri) => { 
-        setImageUris(imageUris.filter(imageUri => imageUri !== uri))
-    }
+   
 
  
     return (
-        <Screen>
-           
-            <ImageInputList
-             imageUris={imageUris} 
-             onAddImage={handleAdd}
-             onRemoveImage={handleRemove}
-             />
-        </Screen>
+        <ListEditScreen  />
     );
 }
